eec4ae798015a2c12da8b112042111a954084b74
Morten <Morten@Morten-PC>
2021-10-23 14:57:44 +0200

    Different words started

---
+++ Opgave2/src/NoOfDifferentWords.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class NoOfDifferentWords {
+
+	public static void main(String[] args) 
+		throws FileNotFoundException {
+			Scanner input = new Scanner(new File("H:text17_00.txt"));
+			int differentwords=0;
+			
+			
+
+			
+			System.out.println("count is: " + differentwords);
+			
+		}
+		
+
+
+}
